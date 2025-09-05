8e082e9a3e4450af2fecc2deb78e53a42e48cbc5
sank-8 <sanket.unifins@gmail.com>
2025-09-06 00:07:15 +0530

    change base to /

--- main/vite.config.ts
+++ main/vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "main",
+  base: "/",
   plugins: [
     react(),
     tailwindcss(),
